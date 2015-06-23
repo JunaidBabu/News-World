c7ad283e346ec8c1fd88eb0bd9ce1901f6608a78
Junaid Babu <junaidvayoli@gmail.com>
2015-06-23 18:33:49 +0530

    some working code. moving along in the map, getting the street name, etc. Need to GET the news with streetname, improve the layout and smoothen the transition

--- mobile/src/main/java/com/example/sony/newsworld/MainActivity.java
+++ mobile/src/main/java/com/example/sony/newsworld/MainActivity.java
@@ -1,37 +1,152 @@
 package com.example.sony.newsworld;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends Activity implements OnMapReadyCallback {
+
+    GoogleMap unimap;
+    float zoomLevel =1;
+    //LatLng ltlng;
+    float lat=10, lng=80;
+    String streetName="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        zoomLevel = 1;
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    public void Clicked(View v){
+        moveMap(v.getTag().toString());
+       // Toast.makeText(this, v.getTag().toString(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    public void onMapReady(GoogleMap map) {
+        unimap = map;
+        LatLng sydney = new LatLng(lat, lng);
+
+        //map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoomLevel));
+
+//        map.addMarker(new MarkerOptions()
+//                .title("Sydney")
+//                .snippet("The most populous city in Australia.")
+//                .position(sydney));
+    }
+
+    public void moveMap(String key){
+        //map.setMyLocationEnabled(true);
+
+        if(key.equals("19")) { //up
+            //Toast.makeText(this, "up", Toast.LENGTH_SHORT).show();
+            lat++;
+        }else if(key.equals("20")){ //down
+            lat--;
+           // Toast.makeText(this, "down", Toast.LENGTH_SHORT).show();
+
+        }else if(key.equals("21")){ //left
+            lng--;
+            //Toast.makeText(this, "left", Toast.LENGTH_SHORT).show();
+
+        }else if(key.equals("22")){ //right
+            lng++;
+            //Toast.makeText(this, "right", Toast.LENGTH_SHORT).show();
+
+        }
+        LatLng ltlng = new LatLng(lat, lng);
+        unimap.moveCamera(CameraUpdateFactory.newLatLngZoom(ltlng, zoomLevel));
+
+        new RequestTask().execute("http://maps.googleapis.com/maps/api/geocode/json?latlng=+"+lat+","+lng+"+&sensor=true");
+        Log.i("url", "http://maps.googleapis.com/maps/api/geocode/json?latlng=+"+lat+","+lng+"+&sensor=true");
+    }
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        Toast.makeText(this, String.valueOf(event.getKeyCode()), Toast.LENGTH_SHORT).show();
+
+        LatLng sydney = new LatLng(-33.867, 151.206);
+        zoomLevel++;
+        //map.setMyLocationEnabled(true);
+        unimap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoomLevel));
+
+        Log.i("key pressed", String.valueOf(event.getKeyCode()));
         return true;
+        //return super.dispatchKeyEvent(event);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+    public class RequestTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... uri) {
+            URL url = null;
+            String json = "{'knockkock': 'null here'}";
+
+            try {
+                url = new URL(uri[0]);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                int sc = con.getResponseCode();
+                if (sc == 200) {
+                    InputStream is = con.getInputStream();
+                    json = readResponse(is);
+                    is.close();
+                }
+            } catch (Exception e) {
+            }
+
+            return json;
         }
 
-        return super.onOptionsItemSelected(item);
+        private String readResponse(InputStream is) throws IOException {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            byte[] data = new byte[2048];
+            int len = 0;
+            while ((len = is.read(data, 0, data.length)) >= 0) {
+                bos.write(data, 0, len);
+            }
+            return new String(bos.toByteArray(), "UTF-8");
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            streetName="";
+            try {
+                JSONObject json = new JSONObject(result);
+                streetName=json.getJSONArray("results").getJSONObject(0).getString("formatted_address");
+            } catch (JSONException e) {
+                //e.printStackTrace();
+            }
+            Log.i("Result", streetName);
+            //Do anything with response..
+        }
     }
 }
+
--- tv/src/main/java/com/example/sony/newsworld/MainActivity.java
+++ tv/src/main/java/com/example/sony/newsworld/MainActivity.java
@@ -1,33 +1,61 @@
-/*
- * Copyright (C) 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
-
 package com.example.sony.newsworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-/*
- * MainActivity class that loads MainFragment
- */
-public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MainActivity extends Activity implements OnMapReadyCallback {
+
+    GoogleMap unimap;
+    float zoomlevel;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        zoomlevel = 1;
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        unimap = map;
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        //map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,zoomlevel));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        Toast.makeText(this, String.valueOf(event.getKeyCode()), Toast.LENGTH_SHORT).show();
+
+        LatLng sydney = new LatLng(-33.867, 151.206);
+        zoomlevel++;
+        //map.setMyLocationEnabled(true);
+        unimap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,zoomlevel));
+
+        Log.i("key pressed", String.valueOf(event.getKeyCode()));
+        return true;
+        //return super.dispatchKeyEvent(event);
+    }
+
+
 }
